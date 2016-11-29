ad0149c4e1779fb5a2df8bfa4b40f88fea95a9b2
alex-zwingli <alex.zwingli@outlook.com>
2016-11-29 10:32:26 -0700

    Add Client Portal Area

---
+++ NWLabs/NWLabs/Areas/ClientPortal/ClientPortalAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace NWLabs.Areas.ClientPortal
+{
+    public class ClientPortalAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "ClientPortal";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "ClientPortal_default",
+                "ClientPortal/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
---
+++ NWLabs/NWLabs/Areas/ClientPortal/Controllers/AccountSummaryController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace NWLabs.Areas.ClientPortal.Controllers
+{
+    public class AccountSummaryController : Controller
+    {
+        // GET: ClientPortal/AccountSummary
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
